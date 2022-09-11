bdcfc41bca747068589b5bec1c22f654601ca9c5
Windmill_City <1449182174@qq.com>
2022-09-11 23:13:51 +0800

    fix siteId undefine

--- assets/gitbook/gitbook-plugin-sharing/buttons.js
+++ assets/gitbook/gitbook-plugin-sharing/buttons.js
@@ -92,9 +92,8 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
         }
 
         // Direct actions to share
-        $.each(SITES, function(sideId, site) {
-            if (!sideId) return;
-            if (!opts[sideId]) return;
+        $.each(SITES, function(siteId, site) {
+            if (!opts[siteId]) return;
 
             var onClick = site.onClick;
 
